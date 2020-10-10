01910229b43c15b65387b9d7ef7653b4c6740459
Alexander Römer <alex@roemer.design>
2020-10-10 20:14:15 +0200

    Initial Commit

---
+++ OnBoardingScreenUnlockSlider/OnBoardingScreenUnlockSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingScreenUnlockSliderApp.swift
+//  OnBoardingScreenUnlockSlider
+//
+//  Created by Alexander Römer on 10.10.20.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingScreenUnlockSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
